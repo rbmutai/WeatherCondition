e03450cda80666f231da3693074d632c21872974
Robert Mutai <rbmutai@gmail.com>
2025-01-13 22:00:05 +0300

    add api service methods

--- WeatherCondition/WeatherViewModel.swift
+++ WeatherCondition/WeatherViewModel.swift
@@ -24,4 +24,120 @@ class WeatherViewModel {
     @Published var conditions: String = ""
     @Published var weatherTheme: ConditionTheme = .clear
     @Published var forcastDetail: [ForcastDetail] = []
+    
+    let apiService: APIServiceProtocol
+    
+    init(apiService: APIServiceProtocol) {
+        self.apiService = apiService
+    }
+    
+    func getWeather (latitude: Double, longitude: Double) async {
+        
+            do {
+               
+                showActivityIndicator = true
+                
+                async let currentWeather = apiService.fetchCurrentWeather(latitude: latitude, longitude: longitude)
+                
+                async let weatherForcast = apiService.fetchWeatherForcast(latitude: latitude, longitude: longitude)
+               
+                let (current, forcast) = try await (currentWeather, weatherForcast)
+                
+                updateCurrentWeatherDetails(current: current)
+                
+                updateWeatherForcastDetails(forcast: forcast)
+                
+                showActivityIndicator = false
+                
+            } catch {
+                showActivityIndicator = false
+                
+                switch error {
+                    case ResultError.network:
+                        errorMessage = "Network error"
+                    case ResultError.parsing:
+                        errorMessage = "Parsing error"
+                    case ResultError.data:
+                        errorMessage = "Data error"
+                    default:
+                        errorMessage = "Error: \(error.localizedDescription)"
+                }
+            }
+        
+    }
+    
+    func updateCurrentWeatherDetails(current: Current){
+        
+        currentTemperature = "\(current.main.temp)º"
+        minimumTemperature = "\(current.main.tempMin)º"
+        maximumTemperature = "\(current.main.tempMax)º"
+        conditions = current.weather[0].description.uppercased()
+        weatherTheme = getWeatherTheme(conditions: conditions.lowercased())
+        city = current.name ?? ""
+    }
+    
+    func updateWeatherForcastDetails(forcast:[Current]){
+        forcastDetail = []
+        var previousDay = ""
+        let today = getDayOfWeek(timeStamp: Date().timeIntervalSince1970)
+        
+        for item in forcast {
+             let dayOfWeek = getDayOfWeek(timeStamp: Double(item.dt))
+             let theme = getWeatherTheme(conditions: item.weather[0].description)
+             let temperature = "\(item.main.temp)º"
+            
+             if previousDay != dayOfWeek && dayOfWeek != today {
+                 let forcastItem = ForcastDetail(day: dayOfWeek, theme: theme, temperature: temperature)
+                 forcastDetail.append(forcastItem)
+                 previousDay = dayOfWeek
+             }
+        }
+    }
+    
+    func getDayOfWeek(timeStamp: Double)-> (String) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let currentDate = Date(timeIntervalSince1970: timeStamp)
+        
+        return dateFormatter.string(from: currentDate)
+        
+    }
+    
+    func getWeatherTheme(conditions: String) -> ConditionTheme {
+        if conditions.contains("cloud") {
+            return .cloudy
+        } else if conditions.contains("rain") || conditions.contains("drizzle") || conditions.contains("snow") {
+            return .rainy
+        } else if conditions.contains("clear") {
+            return .clear
+        } else{
+            return .rainy
+        }
+    }
+    
+    func getWeatherIcon(theme: ConditionTheme)-> String {
+        switch theme {
+        case .cloudy:
+            return "partlySunny"
+        case .rainy:
+            return "rain"
+        case .clear:
+            return "clear"
+        }
+    }
+    
+    func getWeatherBackground(theme: ConditionTheme)-> (imageName: String, colorName: String) {
+        switch theme {
+        case .cloudy:
+            return ("forestCloudy", "Cloudy")
+        case .rainy:
+            return ("forestRainy","Rainy")
+        case .clear:
+            return ("forestSunny","Sunny")
+        }
+    }
+    
+    
 }
