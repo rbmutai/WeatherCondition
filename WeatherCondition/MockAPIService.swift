36aae34cf7b476a4e37755cc24bb9ac4974aff04
Robert Mutai <rbmutai@gmail.com>
2025-01-13 19:45:05 +0300

    add mock api service

---
+++ WeatherCondition/MockAPIService.swift
@@ -0,0 +1,49 @@
+//
+//  MockAPIService.swift
+//  WeatherCondition
+//
+//  Created by Robert Mutai on 13/01/2025.
+//
+
+import Foundation
+
+class MockAPIService: APIServiceProtocol {
+    func fetchCurrentWeather(latitude: Double, longitude: Double) async throws -> Current {
+        guard let bundleUrl = Bundle.main.url(forResource: "CurrentWeather", withExtension: "json") else {
+            throw ResultError.data }
+        
+        do {
+            let data = try Data(contentsOf: bundleUrl)
+            
+            let decoder = JSONDecoder()
+            
+            let weather = try decoder.decode(Current.self, from: data)
+            
+            return weather
+            
+        } catch {
+            throw ResultError.parsing
+        }
+    }
+    
+    func fetchWeatherForcast(latitude: Double, longitude: Double) async throws -> [Current] {
+        guard let bundleUrl = Bundle.main.url(forResource: "ForcastWeather", withExtension: "json") else {
+            throw ResultError.data }
+        
+        do {
+            let data = try Data(contentsOf: bundleUrl)
+            
+            let decoder = JSONDecoder()
+            
+            let weather = try decoder.decode(Forcast.self, from: data)
+            let forcast = weather.list
+            
+            return forcast
+            
+        } catch {
+            throw ResultError.parsing
+        }
+    }
+    
+    
+}
