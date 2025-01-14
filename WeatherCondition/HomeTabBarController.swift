729110cb9f549306704b402861f986cf9a4d5f9b
Robert Mutai <rbmutai@gmail.com>
2025-01-14 10:17:21 +0300

    add home and favourites views and controllers

---
+++ WeatherCondition/HomeTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeTabBarController.swift
+//  WeatherCondition
+//
+//  Created by Robert Mutai on 14/01/2025.
+//
+
+import UIKit
+
+class HomeTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
