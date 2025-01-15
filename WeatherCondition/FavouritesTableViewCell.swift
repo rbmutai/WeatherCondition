26b00b34119d36cdb5ae5f01e68ccf461bb82043
Robert Mutai <rbmutai@gmail.com>
2025-01-15 16:12:41 +0300

    add view components to favourites

---
+++ WeatherCondition/FavouritesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavouritesTableViewCell.swift
+//  WeatherCondition
+//
+//  Created by Robert Mutai on 15/01/2025.
+//
+
+import UIKit
+
+class FavouritesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cityLabel: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- WeatherCondition/FavouritesViewController.swift
+++ WeatherCondition/FavouritesViewController.swift
@@ -6,24 +6,49 @@
 //
 
 import UIKit
-
-class FavouritesViewController: UIViewController {
-
+import MapKit
+class FavouritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var favouritesTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        favouritesTableView.delegate = self
+        favouritesTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! FavouritesTableViewCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") {
+            (action, sourceView, completionHandler) in
+           
+           completionHandler(true)
+            
+        }
+        
+        let viewAction = UIContextualAction(style: .normal, title: "View") {
+            (action, sourceView, completionHandler) in
+           
+           completionHandler(true)
+            
+        }
+        
+   
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction,viewAction])
+       
+        swipeConfiguration.performsFirstActionWithFullSwipe = false
+        
+        return swipeConfiguration
+    }
 
 }
